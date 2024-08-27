f129c1b4171d1c0794d922e8c5fff58b2385ed5b
Mark Aloo <mbaloo@safaricom.co.ke>
2024-08-27 17:31:08 +0300

    fix: combine to server component

--- app/talks/page.tsx
+++ app/talks/page.tsx
@@ -1,7 +1,21 @@
-import Talks from "@/app/talks/talks";
+import FeedCard from "@/components/feed-card";
+import MotionItem from "@/components/motion-item";
+import MotionMasonry from "@/components/motion-masonry";
+import CTASection from "@/sections/cta-section";
+import FooterSection from "@/sections/footer-section";
 import { appURL } from "@/utilities/constants";
-import { profile } from "@/utilities/content";
+import { feed, profile } from "@/utilities/content";
+import { ArrowBackRounded } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   metadataBase: new URL(appURL),
@@ -13,7 +27,40 @@ export const metadata: Metadata = {
 export default function TalksPage() {
   return (
     <main>
-      <Talks />
+      <Paper sx={{ py: 10 }}>
+        <Container maxWidth="md">
+          <Stack
+            sx={{ mb: 8 }}
+            direction="row"
+            alignItems="center"
+            spacing={2}
+            justifyContent="space-between"
+          >
+            <Button
+              startIcon={<ArrowBackRounded />}
+              LinkComponent={Link}
+              href="/"
+            >
+              Home
+            </Button>
+            <Typography sx={{ textAlign: "center" }} variant="h5">
+              Talks
+            </Typography>
+            <Box />
+          </Stack>
+          <MotionMasonry>
+            {feed
+              .filter((feed) => feed.type == "talk")
+              .map((feed) => (
+                <MotionItem key={feed.id}>
+                  <FeedCard feed={feed} />
+                </MotionItem>
+              ))}
+          </MotionMasonry>
+        </Container>
+      </Paper>
+      <FooterSection />
+      <CTASection />
     </main>
   );
 }
