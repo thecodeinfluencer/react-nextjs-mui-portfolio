832a84c53b411c39fd8caf6641d8be74b3fe06a0
Mark Aloo <mbaloo@safaricom.co.ke>
2025-09-13 11:47:38 +0300

    feat: remove genuibot
    chore: update dependencies in package.json
    
    - Bump @emotion packages to latest versions
    - Update @vercel/analytics and @vercel/speed-insights to newer releases
    - Upgrade zod to version 4.1.8
    - Upgrade typescript to version 5.9.2

--- app/genuibot/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AI } from "./actions";
-
-type Props = Readonly<{ children: React.ReactNode }>;
-
-export default function Layout({ children }: Props) {
-  return <AI>{children}</AI>;
-}
